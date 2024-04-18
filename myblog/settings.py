9d44c5d4a49e82bd1c88d68db70cb9a2e2908ed7
d3cod3rgn3d <nexchance@proton.me>
2024-04-18 15:20:20 +0200

    change

--- myblog/settings.py
+++ myblog/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-load_dotenv()
+#load_dotenv()
 
 SECRET_KEY = 'idntcareabutmysecretkey'
 
